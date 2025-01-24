8620319961dca958b5b465c0c06f7e66d5b4c846
believer297 <amaraymen5@gmail.com>
2025-01-24 13:54:10 +0100

    Enhance authentication UI with new layout, login page, and button component; update global styles for improved theming

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-primary to-secondary`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,37 @@
 import { Button } from "@/components/ui/button";
+import { Poppins } from "next/font/google";
+import React from "react";
+import { cn } from "@/lib/utils";
+import { LoginButton } from "@/components/auth/login-button";
+
+const font = Poppins({
+  weight: ["600"],
+  subsets: ["latin"],
+});
 
 export default function Home() {
   return (
-    <div className="">
-      <Button className="" variant={"success"}>
-        {" "}
-        Hello{" "}
-      </Button>
-    </div>
+    <main className="flex flex-col justify-center items-center h-full  ">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl drop-shadow-md font-semibold text-foreground",
+            font.className
+          )}
+        >
+          Auth
+        </h1>
+        <p className="text-foreground capitalize text-lg">
+          simple authentication service
+        </p>
+        <div className="">
+          <LoginButton>
+            <Button className="capitalize" variant={"secondary"} size={"lg"}>
+              sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
