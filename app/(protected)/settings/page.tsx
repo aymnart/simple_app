faba722a5463531c5f1545214a45b747ef25443d
believer297 <amaraymen5@gmail.com>
2025-01-30 17:27:02 +0100

    Implement authentication with NextAuth, including user registration and login functionality; add session handling and middleware for protected routes

---
+++ app/(protected)/settings/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { auth } from "@/auth";
+export default async function SettingsPage() {
+  const session = await auth();
+  return <div>{JSON.stringify(session)}</div>;
+}
