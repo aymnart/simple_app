641632a1965f58e4ffc9e5577747996ae04450c5
believer297 <amaraymen5@gmail.com>
2025-02-22 23:36:08 +0100

    Update Prisma dependencies to version 6.4.1, modify User model to allow optional name field, and add Next.js image configuration for remote patterns

---
+++ next.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  // ...existing code...
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "github.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+    ],
+  },
+  // ...existing code...
+};
