20133583637801eef9597b396b208d2e0309330c
believer297 <amaraymen5@gmail.com>
2025-04-08 18:09:22 +0100

    feat: remove 2fa confirmation token if 2fa is false

--- actions/settings/security.ts
+++ actions/settings/security.ts
@@ -7,25 +7,37 @@ import z from "zod";
 export const updateSecuritySettings = async (
   data: z.infer<typeof securityFormSchema>
 ) => {
-  const session = await auth();
-  if (!session) {
-    return { error: "Unauthorized" };
-  }
-
-  const validatedFields = securityFormSchema.safeParse(data);
-  if (!validatedFields) {
-    throw new Error("Invalid");
-  }
-
-  const user = session.user;
+  const user = (await auth())?.user;
   if (!user || !user.id) {
     return { error: "User not found!" };
   }
 
-  await db.user.update({
-    where: { id: user.id },
-    data: { isTwoFactorEnabled: validatedFields.data?.two_factor },
-  });
+  const validatedFields = securityFormSchema.safeParse(data);
+  if (!validatedFields.success) {
+    throw new Error("Invalid");
+  }
+
+  if (validatedFields.data.two_factor === false) {
+    try {
+      await db.twoFactorConfirmation.delete({
+        where: { userId: user.id },
+      });
+    } catch (error) {
+      console.warn(
+        "Two-factor confirmation record not found or already deleted:",
+        error
+      );
+    }
+    await db.user.update({
+      where: { id: user.id },
+      data: { isTwoFactorEnabled: false },
+    });
+  } else {
+    await db.user.update({
+      where: { id: user.id },
+      data: { isTwoFactorEnabled: validatedFields.data.two_factor },
+    });
+  }
 
   return { success: "Security settings updated successfully!" };
 };
